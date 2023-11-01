74e6796e2bab00c2e2d7084fc77d0d2edcbda5cd
Stephen Gruzin <stephengrzn@gmail.com>
2023-10-31 22:18:09 -0700

    Made a simple websocket client and server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-chat
 
 go 1.21.3
+
+require github.com/gorilla/websocket v1.5.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func websocketHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	defer conn.Close()
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for {
+		_, message, err := conn.ReadMessage()
+
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		log.Printf("Recieved message: %s", message)
+		message2 := append(message, []byte(" pong")...)
+		err = conn.WriteMessage(websocket.TextMessage, message2)
+
+		if err != nil {
+			log.Println(err)
+			break
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Hello World")
+	fs := http.FileServer(http.Dir("./static"))
+
+	http.HandleFunc("/socket", websocketHandler)
+	http.Handle("/", fs)
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }
